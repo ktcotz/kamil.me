b206fc6707d76dea58239c8f3418dc368bbd9ee4
ktcotz <naskret.kamil@gmail.com>
2025-04-10 21:51:04 +0200

    feat: husky, lint-staged
    Add husky,lint-staged,eslint,prettier. Configuration all of them.

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {},
+};
